baa58f56c3d28bab449b8b27d5f487787add628c
xq <2193517652@qq.com>
2020-10-27 16:31:41 +0800

    创建button组件

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
-        button組件
+    button示例
     </div>
+    <h1>Demo1</h1>
+    <ButtonDemo @click="onClick">你好</ButtonDemo>
 </template>
+
+<script lang='ts'>
+import ButtonDemo from '../lib/buttonDemo.vue'
+export default {
+    components: {ButtonDemo},
+    setup() {
+        const onClick = ()=>{
+            console.log('hello')
+        }
+        return {onClick}
+    }
+}
+</script>
---
+++ src/lib/buttonDemo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button v-bind="rest">
+      <slot />
+    </button>
+  </div>
+</template>
+<script lang='ts'>
+export default {
+    inheritAttrs: false,
+    setup(props, ctx){
+       const {size, ...rest} = ctx.attrs
+       return {size, rest}
+    }
+}
+</script>
