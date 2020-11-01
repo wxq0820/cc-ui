39602a3f84401962293214c13f31eaee944d6b91
xq <2193517652@qq.com>
2020-11-01 17:24:05 +0800

    indicator css1

--- src/components/tabs.vue
+++ src/components/tabs.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        tabs組件
-    </div>
+  <div>Tabs示例</div>
+  <h1>Demo1</h1>
+  <TabsDemo v-model:selected="s">
+    <TabDemo title="title1">内容1</TabDemo>
+    <TabDemo title="title2">内容2</TabDemo>
+  </TabsDemo>
 </template>
+<script lang="ts">
+import TabsDemo from '../lib/tabsDemo.vue'
+import TabDemo from '../lib/tabDemo.vue'
+import { ref } from 'vue'
+export default {
+  components: {
+    TabsDemo,
+    TabDemo,
+  },
+  setup(){
+      const s = ref('title1')
+      return{ s }
+  }
+}
+</script>
---
+++ src/lib/tabsDemo.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="gugu-tabs">
+    <div class="gugu-tabs-nav">
+      <div
+        class="gugu-tabs-nav-item"
+        :class="{ selected: t === selected }"
+        v-for="(t, index) in titles"
+        @click="select(t)"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+      <div class="gugu-tabs-nav-item-indicator"></div>
+    </div>
+  </div>
+  <div class="gugu-tabs-content">
+    <component
+      class="gugu-tabs-content-item"
+      :class="{ selected: c.props.title === selected }"
+      v-for="(c, index) in defaults"
+      :key="index"
+      :is="c"
+    />
+  </div>
+</template>
+<script lang="ts">
+import TabDemo from './tabDemo.vue'
+export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
+  setup(props, ctx) {
+    const defaults = ctx.slots.default()
+    defaults.forEach((tag) => {
+      if (tag.type !== TabDemo) {
+        throw new Error('TabsDemo子组件必须是TabDemo')
+      }
+    })
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    const select = (title: string) => {
+      ctx.emit('update:selected', title)
+    }
+    return {
+      defaults,
+      titles,
+      select,
+    }
+  },
+}
+</script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.gugu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+      &-indicator {
+        position: absolute;
+        height: 3px;
+        background: $blue;
+        left: 0;
+        bottom: -1px;
+        width: 100px;
+        transition: all 250ms;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
+</style>
