16b5fe8beda027d227d2084b5c90ee04aa8969f6
xq <2193517652@qq.com>
2020-12-20 18:02:13 +0800

    实现圆弧以及icon

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-chengzi"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"  @click="toggleMenu"></span>
+    <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
 <style lang="scss" scoped>
+$color: #28d1c9;
 .topnav {
-  background-color: pink;
+  color: $color;
   position: relative;
   z-index: 10;
   display: flex;
@@ -20,6 +25,10 @@
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    > svg {
+      width: 32px;
+      height: 32px;
+    }
   }
   > .menu {
     display: flex;
@@ -30,32 +39,38 @@
     }
   }
   > .toggleAside {
-        display: none;
-        width: 24px;
-        height: 24px;
-        background: red;
-        position: absolute;
-        left: 16px;
-        top: 50%;
-        transform: translateY(-50%);
+    display: none;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
     }
-    @media (max-width: 500px) {
-        > .menu {display: none;}
-        > .logo {margin: 0 auto;}
-        > .toggleAside {display: inline-block;}
+    > .logo {
+      margin: 0 auto;
     }
+    > .toggleAside {
+      display: inline-block;
+    }
+  }
 }
 </style>
 <script lang="ts">
 import { inject, Ref } from 'vue'
 
 export default {
-    setup() {
-        const asideVisible = inject<Ref<boolean>>('asideVisible')
-        const toggleMenu = ()=>{
-            asideVisible.value = !asideVisible.value
-        } 
-        return {toggleMenu}
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible')
+    const toggleMenu = () => {
+      asideVisible.value = !asideVisible.value
     }
+    return { toggleMenu }
+  },
 }
 </script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,40 +1,71 @@
 <template>
   <div>
-    <Topnav />
-    <div class="banner">
-      <h1>咕咕UI</h1>
-      <h2>一个厉害的UI 框架</h2>
-      <p class="actions">
-        <a href="">Github</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+    <div class="topnavAndBanner">
+      <Topnav />
+      <div class="banner">
+        <h1>咕咕UI</h1>
+        <h2>一个厉害的UI 框架</h2>
+        <p class="actions">
+          <a href="">Github</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
     </div>
+  </div>
+  <div class="features">
+    <svg class="icon">
+      <use xlink:href="#icon-Vue"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-ts"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-deng"></use>
+    </svg>
   </div>
 </template>
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #28d1c9;
+.topnavAndBanner {
+  background: linear-gradient(
+    145deg,
+    rgba(227, 255, 253, 1) 0%,
+    rgba(183, 233, 230, 1) 100%
+  );
+  clip-path: ellipse(80% 60% at 50% 40%);
+}
 .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background-color: lightblue;
   > .actions {
     padding: 8px 0;
     a {
       margin: 0 8px;
-      background-color: #fff;
+      background-color: $green;
+      color: white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover {
+        text-decoration: none;
+      }
     }
+  }
+}
+.features {
+  > svg {
+    width: 64px;
+    height: 64px;
   }
 }
 </style>
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
-export default {components: {Topnav}}
+export default { components: { Topnav } }
 </script>
